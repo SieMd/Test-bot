3d996b14be182913011ce7da72fd2653b1ff7f44
SieMd <150290370+SieMd@users.noreply.github.com>
2024-04-03 15:26:54 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,8 @@
 import discord
-from discord.ext import commands
+from discord.ext import discord
+
+intents = discord.Intents.default()
+intents.message_content = true
 
 prefix = "!"
 
@@ -18,4 +21,4 @@ async def limpar(ctx, quantidade: int):
     await ctx.channel.purge(limit=quantidade+1) # Apaga msgs
 
 
-bot.run('TOKEN DO SEU BOT') # Aqui é pro token
+bot.run('MTIyMzA0MDYzODEwNzI1ODk3MQ.GLjrxB.7EK8BGQYrfgNJqcpodw_yvNyzYw8xlMA4Pypkk')
